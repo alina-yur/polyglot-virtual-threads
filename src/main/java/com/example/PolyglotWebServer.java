ab2a401ffb3f67dc57e608edbe9e17514952bb13
alina-yur <alina.yurenko@oracle.com>
2024-08-20 16:24:45 +0200

    initial commit

---
+++ src/main/java/com/example/PolyglotWebServer.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.nio.charset.StandardCharsets;
+
+
+public class PolyglotWebServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello from Truffle and virtual threads 🩷";
+                exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+                exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
+                try (OutputStream os = exchange.getResponseBody()) {
+                    os.write(response.getBytes(StandardCharsets.UTF_8));
+                }
+            }
+        });
+
+        server.setExecutor(java.util.concurrent.Executors.newVirtualThreadPerTaskExecutor());
+
+        server.start();
+        System.out.println("Server is running on http://localhost:8080/");
+    }
+}
